0cb09034c1b99524639cd36920c2edb5de360937
Minh Pham <mpham@naver.com>
2018-12-20 19:52:39 +0100

    Add review model and reference to place model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_184803) do
+ActiveRecord::Schema.define(version: 2018_12_20_185036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2018_12_20_184803) do
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.bigint "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["place_id"], name: "index_reviews_on_place_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +71,5 @@ ActiveRecord::Schema.define(version: 2018_12_20_184803) do
   add_foreign_key "habits", "users"
   add_foreign_key "notes", "users"
   add_foreign_key "places", "users"
+  add_foreign_key "reviews", "places"
 end
