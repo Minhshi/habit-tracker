339fc0e61d7c2005ad6a315b96a0988d7d1db2c3
Minh Pham <mpham@naver.com>
2018-12-26 10:01:59 +0100

    Add settings to user model

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :reviews, only: :create
   end
   resources :notes
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:edit, :update]
   # get "profile" => "users#show", :as => 'profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
